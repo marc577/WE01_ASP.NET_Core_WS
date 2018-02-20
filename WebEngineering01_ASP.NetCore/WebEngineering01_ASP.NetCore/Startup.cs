91a1a13a0b1c2424013254fbc568e8e7a289835a
Marc <marc@h8m.eu>
2018-02-20 13:29:35 +0100

    ToDoList implemented

--- WebEngineering01_ASP.NetCore/WebEngineering01_ASP.NetCore/Startup.cs
+++ WebEngineering01_ASP.NetCore/WebEngineering01_ASP.NetCore/Startup.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
+using WebEngineering01_ASP.NetCore.Models;
 
 namespace WebEngineering01_ASP.NetCore
 {
@@ -24,6 +26,7 @@ namespace WebEngineering01_ASP.NetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
