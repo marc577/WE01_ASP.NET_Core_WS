157be853a5a5f3ea6ba594e6e4c28b3808673237
Marc <marc@h8m.eu>
2018-02-20 11:12:19 +0100

    Added WebApi Project
    init WebApi
    added Swagger Config

---
+++ WebEngineering01_ASP.NetCore/WebEngineering01_ASP.NetCore/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WebEngineering01_ASP.NetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Web Engineering 01 ASP.NET Core Example API",
+                    Description = "Web Engineering 01 ASP.NET Core Example API",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Abel, Daniel / Krieg, Alexander / Reinke, Marc / Reus, Theresa", Email = "mar.reinke.16@lehre.mosbach.dhbw.de", Url = "mailto://mar.reinke.16@lehre.mosbach.dhbw.de" },
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Engineering 01 ASP.NET Core Example API");
+            });
+
+
+            app.UseMvc();
+        }
+    }
+}
