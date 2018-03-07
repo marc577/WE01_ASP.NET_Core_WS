00e04433b4c4424c264290a29a59cf1601cc3c4c
alexkrieg <alexkrieg93@googlemail.com>
2018-03-07 17:12:24 +0100

    v2testProject

---
+++ CollabTodoListBackendV2/CollabTodoListBackendV2/Controllers/TodoListController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CollabTodoListBackendV2.Models;
+
+namespace CollabTodoListBackendV2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TodoListController:Controller
+    {
+        private readonly TodoListContext _context;
+
+        public TodoListController(TodoListContext context)
+        {
+            _context = context;
+			if(_context.TodoLists.Count() == 0){
+                var to = new TodoList { Name = "List42" };
+                to.Items.Add(new TodoItem{Name = "Item1"});
+                _context.TodoLists.Add(to);
+                _context.SaveChanges();
+                //Console.WriteLine("{0} records saved to database", count);
+            }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<TodoList> GetAll()
+        {
+            //var blog = _context.TodoLists.Single(b => b.Id == 1);
+
+
+
+            var blogs = _context.TodoLists.Include(blog => blog.Items).ToList();
+            return blogs;
+
+            //_context.Entry(blog)
+                //.Reference(b => b.)
+                //.Load();
+            //_context.TodoItems.
+			//return _context.TodoLists.ToList<TodoList>(); 
+   //         return _context.TodoLists.Include(g => g.Teams).Include(g => g.Teams.TeamsList.Select(t => t.Players)).ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
---
+++ CollabTodoListBackendV2/CollabTodoListBackendV2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using CollabTodoListBackendV2.Models;
+
+namespace CollabTodoListBackendV2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //using (var db = new TodoListContext())
+            //{
+                //db.TodoLists.Add(new TodoList { Name = "List42" });
+                //var count = db.SaveChanges();
+                //Console.WriteLine("{0} records saved to database", count);
+
+                //Console.WriteLine();
+                //Console.WriteLine("All blogs in database:");
+                //foreach (var blog in db.Blogs)
+                //{
+                 //   Console.WriteLine(" - {0}", blog.Url);
+                //}
+            //}
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
